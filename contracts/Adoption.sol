465122b5ae4878c63ea486e78f43f70a068c1c00
Srujan Deshpande <srujan.deshpande@gmail.com>
2019-10-27 04:05:39 +0530

    deposit and withdraw

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,5 +1,21 @@
 pragma solidity ^0.5.0;
 
+contract CommunityChest {
+    function withdraw() public {
+        msg.sender.transfer(address(this).balance);
+    }
+
+    function deposit(uint256 amount) payable public {
+        require(msg.value == amount);
+        // nothing else to do!
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
+
+
 contract Adoption {
   address[16] public adopters;
   // Adopting a pet
